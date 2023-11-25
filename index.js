058c087996261910e001cc0f079a7721d8279889
Joseph Kemper <JosephKemper@gmail.com>
2023-11-24 19:01:16 -0700

    Built initial html and js files.

---
+++ index.js
@@ -0,0 +1,17 @@
+import ArcGISMap from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+
+const map = new ArcGISMap ({
+    basemap: "topo"
+});
+
+const view = new MapView({
+    map,
+    container: "viewDiv",
+    center: [-118, 34],
+    zoom: 8
+});
+
+view.when(()=>{
+    console.log("view ready")
+});
